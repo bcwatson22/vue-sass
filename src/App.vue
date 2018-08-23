48fc4245f456f444a5f513c338836ed43c0feadb
Billy Watson <watson@Snowmen.home>
2018-08-23 08:58:42 +0100

    initial commit of vue boilerplate with sass. cli v 3.0.0-beta.11, node v 8.11.2

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <CrackOn msg="Get cracking!"/>
+  </div>
+</template>
+
+<script>
+  import CrackOn from './components/CrackOn.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      CrackOn
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin-top: 60px;
+
+    img {
+      max-width: 200px;
+      margin: 0 auto;
+    }
+  }
+</style>
---
+++ src/components/CrackOn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import './../assets/styles/sass/_variables.scss';
+
+  h1 {
+    color: $purple;
+  }
+</style>
